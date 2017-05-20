fcb2485851d8f9f861388dbf1f45e31856fad8bf
Nick Lonsway <nicholas.lonsway@aa.com>
2017-05-20 12:32:35 -0500

    Add POST request to AWS

--- app/src/main/java/com/example/rest/Product.java
+++ app/src/main/java/com/example/rest/Product.java
@@ -5,6 +5,11 @@ public class Product {
     private String sku;
     private String quantity;
 
+    public Product(final String sku, final String quantity) {
+        this.sku = sku;
+        this.quantity = quantity;
+    }
+
     public String getSku() {
         return this.sku;
     }
